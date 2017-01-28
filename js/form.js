c8cd34e44e8faf1f0dd8c776367643c3d7830821
IcyCrumb <ludysh21@gmail.com>
2017-01-28 03:17:21 +0300

    Перемещение активности элемента

--- js/form.js
+++ js/form.js
@@ -1,1 +1,14 @@
+'use strict';
 
+var items = document.querySelectorAll(".pin");
+
+for (var i = 0, len = items.length; i < len; i++) {
+  items[i].addEventListener('click', set_handler(i), false);
+}
+
+function set_handler(i) {
+  return function(e) {
+    document.querySelector("div.pin.pin--active").classList.remove("pin--active");
+    items[i].classList.add("pin--active");
+  };
+}
