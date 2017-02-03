a72cecf1717c2b7f26df8d77ed5aadea140a3bbc
IcyCrumb <ludysh21@gmail.com>
2017-02-04 01:21:52 +0300

    Revert "Переключение между .pin и открытие/закрытие .dialog с клавиатуры"
    This reverts commit 059d3ceac6a96439972d823d7597e28b1eb0f196.

--- js/form.js
+++ js/form.js
@@ -1,112 +1,29 @@
 'use strict';
 
-var ENTER_KEY_CODE = 13;
-var ESCAPE_KEY_CODE = 27;
+var items = document.querySelectorAll('.pin');
 
-//var isActivateEvent = function(evt) {
-  //return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-//}
-
-// находим все элементы с классом .pin и делаем из них кнопки
-var pins = document.querySelectorAll('.pin');
-
-for (var i = 0; i < pins.length; i++) {
-  pins[i].setAttribute('role', 'button');
-  pins[i].tabIndex = 0;
-  pins[i].setAttribute('aria-pressed', 'false');
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', setHandler(i), false);
 }
 
-// при нажатии клавиатуры или щелчке мышью на элемент .pin срабатывает функция setHandler(k)
-for (var i = 0; i < pins.length; i++) {
-  pins[i].addEventListener('click', setPinHandler(i));
-  //if (evt.keyCode == ENTER_KEY_CODE) {
-    pins[i].addEventListener('keydown', setPinHandler(i));
-  //}
-}
-
-// функция делает текущий элемент активным и убирает "активность" с предыдущего элемента
-function setPinHandler(k) {
+function setHandler(k) {
   return function () {
     var active = document.querySelector('div.pin.pin--active');
     if (active) {
       active.classList.remove('pin--active');
     }
-    pins[k].classList.add('pin--active');
+    items[k].classList.add('pin--active');
     var dialogWindow = document.querySelector('.dialog');
     dialogWindow.style.visibility = 'visible';
   };
 }
 
-// находим элемент с классом .dialog и делаем из него "документ"
-var dialog = document.querySelector('div.dialog');
-dialog.setAttribute('role', 'document');
-
-// находим элемент с классом .dialog__close и делаем из него кнопку
 var buttonCross = document.querySelector('.dialog__close');
-buttonCross.setAttribute('role', 'button');
-buttonCross.tabIndex = 1;
-buttonCross.setAttribute('aria-pressed', 'false');
-
-// при нажатии клавиатуры или щелчке мышью на крестик срабатывает функция setCrossHandler
-buttonCross.addEventListener('click', setCrossHandler);
-//if (evt.keyCode == ENTER_KEY_CODE) {
-  buttonCross.addEventListener('keydown', setCrossHandler);
-//}
-
-// функция закрывает элемент .dialog и деактивирует элемент .pin
-function setCrossHandler() {
-  //if (evt.keyCode === ENTER_KEY_CODE) {
-    var dialogWindow = document.querySelector('.dialog');
-    dialogWindow.style.visibility = 'hidden';
-    document.querySelector('div.pin.pin--active').classList.remove('pin--active');
-  //}
-};
-
-
-
-
-/*
 buttonCross.addEventListener('click', function () {
   var dialogWindow = document.querySelector('.dialog');
   dialogWindow.style.visibility = 'hidden';
   document.querySelector('div.pin.pin--active').classList.remove('pin--active');
 });
-*/
-/*
-var dialog = document.querySelector('div.dialog');
-//dialog.setAttribute('role', 'dialog');
-
-var isActivateEvent = function(evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-}
-
-var setupKeydownHandler = function(evt) {
-  if (evt.target !== document.querySelector('') && evt.keyCode === ESCAPE_KEY_CODE) {
-    pins.classList.add('invisible');
-  }
-}
-
-var showSetupElement = function () {
-  pins.classList.remove('invisible');
-  document.addEventListener('keydown', setupKeydownHandler);
-}
-
-var hideSetupElement = function () {
-  pins.classList.add('invisible');
-  document.removeEventListener('keydown', setupKeydownHandler);
-}
-
-.addEventListener('click', function () {
-  showSetupElement;
-});
-
-.addEventListener('keydown', function(evt) {
-  if (isActivateEvent(evt)) {
-    showSetupElement;
-  }
-});
-*/
-
 
 var title = document.getElementById('title');
 title.required = true;
