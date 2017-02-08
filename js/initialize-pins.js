6c8e026a93c0c5dd9ada7e5f04b0f80dc83b8262
IcyCrumb <ludysh21@gmail.com>
2017-02-08 19:58:44 +0300

    Обернула отрисовку меток в функцию и вынесла её в этот файл

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,61 +1,14 @@
-/*var buttonCross = document.querySelector('.dialog__close');
-var dialogWindow = document.querySelector('.dialog');
-var activeElement = document.querySelector('div.pin.pin--active')
-var pinMap = document.querySelector('.tokyo__pin-map');
-*/
+function initializePins() {
 
-
-//;(function() {
-function initializePins(button, dialogState, dialog, active) {
-
-
-  // при щелчке мышью на крестик срабатывает функция hideDialog()
-  button.addEventListener('click', clickHandler);
-
-   function clickHandler () {
-     //var target = evt.target;
-     dialog.style.visibility = dialogState;
-     //target.setAttribute('aria-pressed', 'true');
-    //activeElement.classList.remove('pin--active');
-     if (active) {
-       active.classList.remove('pin--active');
-       //target.setAttribute('aria-pressed', 'false');
-     }
-     //target.classList.add('pin--active');
-     //return;
-    pinMap.classList.add('pin--active');
-   };
-//};
-window.initializePins = initializePins;
-}//());
-/*
-  var clickHandler = function (evt) {
-    var target = evt.target;
-
-    while (target !== document.body) {
-      if (target.classList.contains('pin')) {
-        if (target.classList.contains('pin--active')) {
-          break;
-        }
-        dialogWindow.style.visibility = 'visible';
-        target.setAttribute('aria-pressed', 'true');
-
-        if (activeElement) {
-          activeElement.classList.remove('pin--active');
-          target.setAttribute('aria-pressed', 'false');
-        }
-
-        target.classList.add('pin--active');
-        return;
-      }
-      target = target.parentNode;
-    }
+  // функция делает элемент с классом .dialog видимым
+  var showDialog = function () {
+    document.querySelector('.dialog').style.visibility = 'visible';
   };
 
-
-
+  // функция дективирует предыдущий элемент и делает активным текущий
   var activeElementHandler = function (elem) {
     elem.setAttribute('aria-pressed', 'true');
+    var activeElement = document.querySelector('div.pin.pin--active');
 
     if (activeElement) {
       activeElement.classList.remove('pin--active');
@@ -64,52 +17,53 @@ window.initializePins = initializePins;
 
     elem.classList.add('pin--active');
   };
-}
 
-// находим элемент с классом .tokyo__pin-map
-var pinMap = document.querySelector('.tokyo__pin-map');
+  // функция вызывает showDialog() и activeElementHandler()
+  // при клике на любой из потомков элемента pinMap
+  var clickHandler = function (evt) {
+    var target = evt.target;
 
-
-// функция дективирует предыдущий элемент и делает активным текущий
-var activeElementHandler = function (elem) {
-  elem.setAttribute('aria-pressed', 'true');
-  var activeElement = document.querySelector('div.pin.pin--active');
-
-  if (activeElement) {
-    activeElement.classList.remove('pin--active');
-    elem.setAttribute('aria-pressed', 'false');
-  }
-
-  elem.classList.add('pin--active');
-};
-
-// функция вызывает showDialog() и activeElementHandler()
-// при клике на любой из потомков элемента pinMap
-var clickHandler = function (evt) {
-  var target = evt.target;
-
-  while (target !== document.body) {
-    if (target.classList.contains('pin')) {
-      if (target.classList.contains('pin--active')) {
-        break;
+    while (target !== document.body) {
+      if (target.classList.contains('pin')) {
+        if (target.classList.contains('pin--active')) {
+          break;
+        }
+        showDialog();
+        activeElementHandler(target);
+        return;
       }
-      dialogWindow.style.visibility = 'visible';
-      activeElementHandler(target);
-      return;
+      target = target.parentNode;
     }
-    target = target.parentNode;
-  }
-};
+  };
 
-// функция вызывает showDialog() и activeElementHandler()
-// при нажатии enter-ом на любой из потомков элемента pinMap
-var keydownHandler = function (evt) {
-  if (evt.keyCode === ENTER_KEY_CODE) {
-    evt.target.click();
-  }
-};
+  // функция вызывает showDialog() и activeElementHandler()
+  // при нажатии enter-ом на любой из потомков элемента pinMap
+  var keydownHandler = function (evt) {
+    if (evt.keyCode === ENTER_KEY_CODE) {
+      evt.target.click();
+    }
+  };
 
-// вызываем  clickHandler() при клике и keydownHandler() при нажатии на pinMap
-pinMap.addEventListener('click', clickHandler);
-pinMap.addEventListener('keydown', keydownHandler);
-*/
+  // вызываем  clickHandler() при клике и keydownHandler() при нажатии на pinMap
+  pinMap.addEventListener('click', clickHandler);
+  pinMap.addEventListener('keydown', keydownHandler);
+
+
+  // при щелчке мышью на крестик срабатывает функция hideDialog()
+  buttonCross.addEventListener('click', function () {
+    buttonCross.setAttribute('aria-pressed', 'true');
+    hideDialog();
+  });
+
+  // функция закрывает элемент .dialog и деактивирует элемент .pin
+  var hideDialog = function () {
+    var dialogWindow = document.querySelector('.dialog');
+    dialogWindow.style.visibility = 'hidden';
+    activeElement = document.querySelector('div.pin.pin--active');
+    if (activeElement) {
+      activeElement.classList.remove('pin--active');
+    }
+  };
+
+window.initializePins = initializePins;
+}
