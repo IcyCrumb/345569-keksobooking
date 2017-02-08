36144d39eb7568fdc19caf8638ed4a2ceb94387b
IcyCrumb <ludysh21@gmail.com>
2017-02-09 02:04:49 +0300

    Вынесла синхронизацию форм в отдельную функцию в этот файл

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,31 +1,15 @@
-/*elem1 = var selectType = document.getElementById('type');
-elem2 = var price = document.getElementById('price');
-array1 = ['квартира', 'лачуга', 'дворец'];
-array2 = [1000, 0, 10000];
-property = 'min';
-*/
+'use strict';
 
-;(function synchronizeFields (elem1, elem2, arr1, arr2, prop) {
+function synchronizeFields (firstForm, secondForm, firstArray, secondArray, property) {
 
-elem1.addEventListener('change', function () {
-  if (elem1.value === arr1[0]) {
-    elem2.setAttribute(prop, arr2[0]);
-  } else if (elem1.value === arr1[1]) {
-    elem2.setAttribute(prop, arr2[1]);
-  } else if (elem1.value === arr1[2]) {
-    elem2.setAttribute(prop, arr2[2]);
-  }
-});
-
+  firstForm.addEventListener('change', function () {
+    for (var i = 0; i < secondArray.length; i++) {
+      if (firstForm.value === firstArray[i]) {
+        secondForm[property] = secondArray[i];
+        break;
+      }
+    }
+  });
 
   window.synchronizeFields = synchronizeFields;
-}());
-
-
-//for (var i = 0; i < elem1.length; i++) {
-  //elem2.setAttribute(property, array2[i])
-//}
-
-//array1.forEach(function(item, i, array1) {
-//elem2.setAttribute(property, array2[i]);
-//});
+};
