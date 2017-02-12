4f12752be9bac6dd1fabcdfca762c9a90010c96b
IcyCrumb <ludysh21@gmail.com>
2017-02-12 16:32:48 +0300

    Убрала обработчик клика после скрытия формы

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -9,12 +9,12 @@ window.initializePins = (function () {
   var dialogWindow = document.querySelector('.dialog');
 
   // функция делает элемент с классом .dialog видимым
-  var showDialog = function () {
+  function showDialog() {
     dialogWindow.style.visibility = 'visible';
-  };
+  }
 
   // функция дективирует предыдущий элемент и делает активным текущий
-  var activeElementHandler = function (elem) {
+  function activeElementHandler(elem) {
     elem.setAttribute('aria-pressed', 'true');
     var activeElement = document.querySelector('div.pin.pin--active');
 
@@ -24,11 +24,11 @@ window.initializePins = (function () {
     }
 
     elem.classList.add('pin--active');
-  };
+  }
 
   // функция вызывает showDialog() и activeElementHandler()
   // при клике на любой из потомков элемента pinMap
-  var clickHandler = function (evt) {
+  function clickHandler(evt) {
     var target = evt.target;
 
     while (target !== document.body) {
@@ -42,33 +42,38 @@ window.initializePins = (function () {
       }
       target = target.parentNode;
     }
-  };
+  }
 
   // функция вызывает showDialog() и activeElementHandler()
   // при нажатии enter-ом на любой из потомков элемента pinMap
-  var keydownHandler = function (evt) {
+  function keydownHandler(evt) {
     if (evt.keyCode === ENTER_KEY_CODE) {
       evt.target.click();
     }
-  };
+  }
 
   // вызываем  clickHandler() при клике и keydownHandler() при нажатии на pinMap
   pinMap.addEventListener('click', clickHandler);
   pinMap.addEventListener('keydown', keydownHandler);
 
 
-  // при щелчке мышью на крестик срабатывает функция hideDialog()
-  buttonCross.addEventListener('click', function () {
+  // обработчик клика на крестик
+  function crossClickHandler() {
     buttonCross.setAttribute('aria-pressed', 'true');
     hideDialog();
-  });
+  }
 
   // функция закрывает элемент .dialog и деактивирует элемент .pin
-  var hideDialog = function () {
+  function hideDialog() {
     dialogWindow.style.visibility = 'hidden';
     var activeElement = document.querySelector('div.pin.pin--active');
     if (activeElement) {
       activeElement.classList.remove('pin--active');
     }
-  };
+    buttonCross.removeEventListener('click', crossClickHandler);
+  }
+
+  // вызываем crossClickHandler() при нажатии на крестик
+  buttonCross.addEventListener('click', crossClickHandler);
+  buttonCross.addEventListener('keydown', crossClickHandler);
 });
