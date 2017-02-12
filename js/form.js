444ecebe33133a18392f49536e89972bb83fa951
IcyCrumb <ludysh21@gmail.com>
2017-02-12 16:32:07 +0300

    Инкапсуляция

--- js/form.js
+++ js/form.js
@@ -1,47 +1,49 @@
 'use strict';
-/* global initializePins synchronizeFields */
 
-window.initializePins();
+window.form = (function () {
 
-
-var title = document.getElementById('title');
-title.required = true;
-title.minLength = 30;
-title.maxLength = 100;
-
-var address = document.getElementById('address');
-address.required = true;
+  window.initializePins();
 
 
-var selectType = document.getElementById('type');
-var price = document.getElementById('price');
-var availableAccommodationTypes = ['Квартира', 'Лачуга', 'Дворец'];
-var availablePrices = [1000, 0, 10000];
-var currentProperty = 'min';
+  var title = document.getElementById('title');
+  title.required = true;
+  title.minLength = 30;
+  title.maxLength = 100;
 
-price.required = true;
-price.setAttribute('min', 1000);
-price.setAttribute('max', 1000000);
+  var address = document.getElementById('address');
+  address.required = true;
 
-window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, currentProperty);
-window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
 
-var selectTimeIn = document.getElementById('time');
-var selectTimeOut = document.getElementById('timeout');
+  var selectType = document.getElementById('type');
+  var price = document.getElementById('price');
+  var availableAccommodationTypes = ['Квартира', 'Лачуга', 'Дворец'];
+  var availablePrices = [1000, 0, 10000];
+  var currentProperty = 'min';
 
-var availableTimein = ['12', '13', '14'];
-var availableTimeout = ['12', '13', '14'];
-currentProperty = 'value';
+  price.required = true;
+  price.setAttribute('min', 1000);
+  price.setAttribute('max', 1000000);
 
-window.synchronizeFields(selectTimeIn, selectTimeOut, availableTimein, availableTimeout, currentProperty);
-window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
+  window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, currentProperty);
+  window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
 
-var selectRoomNumber = document.getElementById('room_number');
-var selectCapacity = document.getElementById('capacity');
+  var selectTimeIn = document.getElementById('time');
+  var selectTimeOut = document.getElementById('timeout');
 
-var availableRoomNumber = ['1 комната', '2 комнаты', '100 комнат'];
-var availableCapacity = ['не для гостей', 'для 3 гостей'];
-currentProperty = 'value';
+  var availableTimein = ['12', '13', '14'];
+  var availableTimeout = ['12', '13', '14'];
+  currentProperty = 'value';
 
-window.synchronizeFields(selectRoomNumber, selectCapacity, availableRoomNumber, availableCapacity, currentProperty);
-window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
+  window.synchronizeFields(selectTimeIn, selectTimeOut, availableTimein, availableTimeout, currentProperty);
+  window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
+
+  var selectRoomNumber = document.getElementById('room_number');
+  var selectCapacity = document.getElementById('capacity');
+
+  var availableRoomNumber = ['1 комната', '2 комнаты', '100 комнат'];
+  var availableCapacity = ['не для гостей', 'для 3 гостей'];
+  currentProperty = 'value';
+
+  window.synchronizeFields(selectRoomNumber, selectCapacity, availableRoomNumber, availableCapacity, currentProperty);
+  window.synchronizeFields(selectType, price, availableAccommodationTypes, availablePrices, 'placeholder');
+})();
