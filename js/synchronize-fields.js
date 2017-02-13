e4ba7b9084833ccb9f4eaaecab3be325d15c124a
IcyCrumb <ludysh21@gmail.com>
2017-02-13 20:49:43 +0300

    Сделала провильную синхронизацию количества комнат и вместимости

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,9 +3,9 @@
 window.synchronizeFields = (function () {
   function synchronizeFields(firstForm, secondForm, firstArray, secondArray, property) {
     firstForm.addEventListener('change', function () {
-      for (var i = 0; i < secondArray.length; i++) {
+      for (var i = 0; i < firstArray.length; i++) {
         if (firstForm.value === firstArray[i]) {
-          secondForm[property] = secondArray[i];
+          (secondForm[property] = secondArray[i]) || (secondForm[property] = secondArray[i-1]);
           break;
         }
       }
