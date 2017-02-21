8b7d185df365ad37a6a2b42c5b6b18980adcf007
IcyCrumb <ludysh21@gmail.com>
2017-02-22 02:48:02 +0300

    задания 1-5,7

--- js/load.js
+++ js/load.js
@@ -8,21 +8,41 @@ window.load = (function () {
     // Создаём новый объект XMLHttpRequest
     var xhr = new XMLHttpRequest();
 
-    // Конфигурируем его: GET-запрос на URL, передающийся в load() в качестве параметра
-    xhr.open('GET', url);
-    //xhr.withCredentials = true;
-    //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    // Отсылаем запрос
-    xhr.send();
+    xhr.open('GET', url, true);
+    xhr.onload = function(event){
+      //document.write(event.target.responseText);
 
-    // Если код ответа сервера не 200, то это ошибка
-    if (xhr.status != 200) {
-    // обработать ошибку
-      alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-    } else {
-      smth(xhr.responseText);
-      //alert( xhr.responseText );
-   }
+      var similarApartments = JSON.parse(event.target.responseText);
+      //console.log(similarApartments[0].offer.checkout);
+
+      document.getElementsByClassName('tokyo')[0].insertAdjacentHTML('afterbegin', '<template id="pin-template"><div class="pin"><img src="img/main-pin-image.png" alt="" class="rounded" width="40" height="40"></div></template>');
+
+      var pins = document.querySelectorAll('.pin');
+      var tokios = document.querySelectorAll('.tokyo__pin-map');
+      for (var i = 1; i < pins.length; i++) {
+        tokios[0].removeChild(pins[i]);
+      }
+
+      var newElements = [];
+
+      for (var j = 0; j < 3; j++) {
+        var templateElement = document.querySelector('#pin-template');
+        var elementToClone = templateElement.content.querySelector('.pin');
+        newElements.push(elementToClone.cloneNode(true));
+
+        newElements[j].style.position = "absolute";
+        var x = similarApartments[j].location.x;
+        var y = similarApartments[j].location.y;
+        newElements[j].style.top = y + "px";
+        newElements[j].style.left = x + "px";
+        //newElements[j].setAttribute('offsetLeft', x);
+        //newElements[j].setAttribute('offsetTop', y);
+
+        tokios[0].appendChild(newElements[j]);
+      }
+    }
+
+    xhr.send();
  }
  return load;
 })();
