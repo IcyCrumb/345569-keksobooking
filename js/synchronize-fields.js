5c4c6b08480530025a0d636ae551626ba120e0fb
IcyCrumb <ludysh21@gmail.com>
2017-02-28 23:19:30 +0300

    Финальные исправления 2

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -4,7 +4,7 @@ window.synchronizeFields = (function () {
 
   function synchronizeFields(firstForm, secondForm, firstArray, secondArray, callback) {
     firstForm.addEventListener('change', function () {
-        callback(secondForm, secondArray[firstArray.indexOf(firstForm.value)]);
+      callback(secondForm, secondArray[firstArray.indexOf(firstForm.value)]);
     });
   }
   return synchronizeFields;
